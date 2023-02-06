19ff8d21c7b25069fb9c9ec8b0448324c037a27f
jonathanchang <cpk826@gmail.com>
2023-02-06 14:57:56 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import DemoWidget from '../components/demo-widget'
 
 const Home: NextPage = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 h-[1000px]">
       <DemoWidget />
     </div>
   )
